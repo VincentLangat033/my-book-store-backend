8ee6f0b2bc04a66322710ce9dadc5684544736d3
VincentLangat033 <kimutaiketer033@gmail.com>
2023-04-26 21:28:52 +0300

    update log files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
+skip_before_action :verify_authenticity_token
 skip_before_action :authorized!, only: [:create]
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     
 
     def create
         user = User.create!(user_params)
-        session[:user_id] = user.id
-        render json: UserSerializer.new(user), status: :created 
+        # session[:user_id] = user.id
+        # render json: UserSerializer.new(user), status: :created 
+        render json: user, status: :created
+            rescue ActiveRecord::RecordInvalid => e
+              render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+            # else
+            #   render json: user.errors.full_messages, status: :unprocessable_entity
+            # end
+          end
     end
 
     def show
